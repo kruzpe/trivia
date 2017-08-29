82fd1695b6b12282113651a95fd4ea239667f4f8
Kruzpe <bruce.kruz@gmail.com>
2017-08-29 15:09:42 -0500

    Initial Commit

---
+++ TrueFalseStarter/QuestionProvider.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionProvider.swift
+//  TrueFalseStarter
+//
+//  Created by jose Andrade Cruz on 28/08/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct Questions {
+    let trivia: [[String : String]] = [
+        ["Question": "Only female koalas can whistle", "Answer": "False"],
+        ["Question": "Blue whales are technically whales", "Answer": "True"],
+        ["Question": "Camels are cannibalistic", "Answer": "False"],
+        ["Question": "All ducks are birds", "Answer": "True"]
+    ]
+}
